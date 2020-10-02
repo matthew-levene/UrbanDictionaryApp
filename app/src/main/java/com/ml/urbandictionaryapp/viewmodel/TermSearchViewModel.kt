6f3da177a17bb8a03f562bac16adf827dabdc3d8
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-02 15:29:54 +0100

    Implement TermSearchViewModel class

---
+++ app/src/main/java/com/ml/urbandictionaryapp/viewmodel/TermSearchViewModel.kt
@@ -0,0 +1,24 @@
+package com.ml.urbandictionaryapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.ml.urbandictionaryapp.repository.TermRepository
+import kotlinx.coroutines.*
+
+class TermSearchViewModel(
+    private val termRepository:TermRepository
+) : ViewModel()
+{
+    private val supervisor = SupervisorJob()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + supervisor)
+
+    fun findDefinitions(term:String){
+        coroutineScope.launch {
+            termRepository.getDefinitions(term)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+}
