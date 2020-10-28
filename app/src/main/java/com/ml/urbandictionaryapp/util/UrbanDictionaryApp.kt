9351262c373117858c64241338bd0e79ec78ce92
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-28 09:30:45 +0000

    Refactor MyApplication class

--- app/src/main/java/com/ml/urbandictionaryapp/util/UrbanDictionaryApp.kt
+++ app/src/main/java/com/ml/urbandictionaryapp/util/UrbanDictionaryApp.kt
@@ -1,18 +1,19 @@
-package com.ml.urbandictionaryapp.di
+package com.ml.urbandictionaryapp.util
 
 import android.app.Application
+import com.ml.urbandictionaryapp.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
-class MyApplication : Application() {
+class UrbanDictionaryApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            androidContext(this@MyApplication)
+            androidContext(this@UrbanDictionaryApp)
             androidLogger(Level.ERROR)
             modules(listOf(networkModule, apiModule, databaseModule, repoModule, viewModelModule))
         }
